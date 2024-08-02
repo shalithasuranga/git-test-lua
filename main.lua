7d7c4f677c84beb11ae4e2e141126eded3df863f
Shalitha Suranga <shalithasuranga@gmail.com>
2024-08-02 12:40:42 +0530

    Display the sum of numbers

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
+function sum(a, b)
+    return a + b
+end
+
 local a, b = io.read("*n", "*n")
+
 if(a == nil or b == nil) then
     print("Invalid inputs, only numbers are accepted.")
     return
 end
 
-print(a, b)
+print(string.format("%d + %d = %d", a, b, sum(a, b)))
